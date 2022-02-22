fa39ee525e70fe3898246fe53de4d08358fe3e01
Alexander Kumanov <kumana@abv.bg>
2022-02-22 17:30:05 +0200

    added messaging system and inbox for users

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile, Skill
+from .models import Profile, Skill, Message
 from django import forms
 
 
@@ -68,4 +68,15 @@ class SkillForm(ModelForm):
         super(SkillForm, self).__init__(*args, **kwargs)
 
         for _, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'email', 'subject', 'body']
+    
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from pyexpat.errors import messages
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
@@ -133,3 +134,64 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    recipient = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="messages"
+    )
+
+    name = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+
+    email = models.EmailField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+
+    subject = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+
+    body = models.TextField(
+
+    )
+
+    is_read = models.BooleanField(
+        default=False,
+        null=True
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+
+    def __str__(self):
+        return self.subject    
+    
+    class Meta:
+        ordering = ['is_read', '-created']
