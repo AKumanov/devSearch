61ab158525e32ced2c8b04c29d4165576770de12
Alexander Kumanov <kumana@abv.bg>
2022-02-24 07:43:11 +0200

    added post request view to vote on a project

--- api/urls.py
+++ api/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
 
     path('', views.getRoutes),
     path('projects/', views.get_projects),
-    path('projects/<str:pk>', views.get_project),
+    path('projects/<str:pk>/', views.get_project),
+    path('projects/<str:pk>/vote/', views.project_vote),
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response 
 from .serializers import ProjectSerializier
-from projects.models import Project
+from projects.models import Project, Review
 
 
 @api_view(['GET'])
@@ -29,5 +29,24 @@ def get_projects(request):
 @api_view(['GET'])
 def get_project(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializier(project, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def project_vote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner = user,
+        project = project,
+    )
+    review.value = data['value']
+    review.save()
+    project.get_vote_count
+
     serializer = ProjectSerializier(project, many=False)
     return Response(serializer.data)
